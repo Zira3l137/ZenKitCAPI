9155ed6951b8930d23e3afd94c8e8b77bba51824
Luis Michaelis <me@lmichaelis.de>
2023-11-04 14:18:19 +0100

    feat(impl): add `zenkit::Font` APIs

--- include/czenkit/Vector.h
+++ include/czenkit/Vector.h
@@ -6,11 +6,14 @@
 	#include <glm/vec2.hpp>
 	#include <glm/vec3.hpp>
 	#include <glm/vec4.hpp>
+
+extern "C" {
 #endif
 
 typedef struct ZkInternal_Vec2f {
 #ifdef __cplusplus
-	explicit inline ZkInternal_Vec2f(glm::vec2 const& v) : x(v.x), y(v.y) {}
+	inline ZkInternal_Vec2f(glm::vec2 const& v) : x(v.x), y(v.y) {}
+	inline ZkInternal_Vec2f(float x, float y) : x(x), y(y) {}
 #endif
 
 	float x, y;
@@ -18,7 +21,8 @@ typedef struct ZkInternal_Vec2f {
 
 typedef struct ZkInternal_Vec3f {
 #ifdef __cplusplus
-	explicit inline ZkInternal_Vec3f(glm::vec3 const& v) : x(v.x), y(v.y), z(v.z) {}
+	inline ZkInternal_Vec3f(glm::vec3 const& v) : x(v.x), y(v.y), z(v.z) {}
+	inline ZkInternal_Vec3f(float x, float y, float z) : x(x), y(y), z(z) {}
 #endif
 
 	float x, y, z;
@@ -26,8 +30,13 @@ typedef struct ZkInternal_Vec3f {
 
 typedef struct ZkInternal_Vec4f {
 #ifdef __cplusplus
-	explicit inline ZkInternal_Vec4f(glm::vec4 const& v) : x(v.x), y(v.y), z(v.z), w(v.w) {}
+	inline ZkInternal_Vec4f(glm::vec4 const& v) : x(v.x), y(v.y), z(v.z), w(v.w) {}
+	inline ZkInternal_Vec4f(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
 #endif
 
 	float x, y, z, w;
 } ZkVec4f;
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/Font.cc
@@ -0,0 +1,111 @@
+// Copyright © 2023. GothicKit Contributors
+// SPDX-License-Identifier: MIT
+#include "czenkit/Font.h"
+
+ZkFont* ZkFont_load(ZkRead* buf) {
+	if (buf == nullptr) {
+		CZK_LOG_WARN_NULL("ZkFont_load");
+		return nullptr;
+	}
+
+	try {
+		ZkFont obj {};
+		obj.load(buf);
+		return CZK_WRAP_NEW(obj);
+	} catch (std::exception const& exc) {
+		CZK_LOG_ERROR("ZkFont_load() failed: %s", exc.what());
+		return nullptr;
+	}
+}
+
+ZkFont* ZkFont_loadPath(ZkString path) {
+	if (path == nullptr) {
+		CZK_LOG_WARN_NULL("ZkFont_loadPath");
+		return nullptr;
+	}
+
+	try {
+		auto buf = zenkit::Read::from(path);
+
+		ZkFont obj {};
+		obj.load(buf.get());
+		return CZK_WRAP_NEW(obj);
+	} catch (std::exception const& exc) {
+		CZK_LOG_ERROR("ZkFont_loadPath() failed: %s", exc.what());
+		return nullptr;
+	}
+}
+
+ZkFont* ZkFont_loadVfs(ZkVfs* vfs, ZkString name) {
+	if (vfs == nullptr || name == nullptr) {
+		CZK_LOG_WARN_NULL("ZkFont_loadVfs");
+		return nullptr;
+	}
+
+	auto node = vfs->find(name);
+	if (node == nullptr) return nullptr;
+
+	auto rd = node->open_read();
+	return ZkFont_load(rd.get());
+}
+
+void ZkFont_del(ZkFont* slf) {
+	delete slf;
+}
+
+ZkString ZkFont_getName(ZkFont const* slf) {
+	if (slf == nullptr) {
+		CZK_LOG_WARN_NULL("ZkFont_getName");
+		return nullptr;
+	}
+
+	return slf->name.c_str();
+}
+
+uint32_t ZkFont_getHeight(ZkFont const* slf) {
+	if (slf == nullptr) {
+		CZK_LOG_WARN_NULL("ZkFont_getHeight");
+		return 0;
+	}
+
+	return slf->height;
+}
+
+ZkSize ZkFont_getGlyphCount(ZkFont const* slf) {
+	if (slf == nullptr) {
+		CZK_LOG_WARN_NULL("ZkFont_getGlyphCount");
+		return 0;
+	}
+
+	return slf->glyphs.size();
+}
+
+ZkFontGlyph ZkFont_getGlyph(ZkFont const* slf, ZkSize i) {
+	if (slf == nullptr) {
+		CZK_LOG_WARN_NULL("ZkFont_getGlyph");
+		return {0, ZkVec2f {0, 0}, ZkVec2f {0, 0}};
+	}
+
+	if (i >= slf->glyphs.size()) {
+		CZK_LOG_ERROR("ZkFont_getGlyph() failed: index out of range");
+		return {0, ZkVec2f {0, 0}, ZkVec2f {0, 0}};
+	}
+
+	auto& glyph = slf->glyphs[i];
+	return {glyph.width, ZkVec2f {glyph.uv[0]}, ZkVec2f {glyph.uv[1]}};
+}
+
+void ZkFont_enumerateGlyphs(ZkFont const* slf, ZkFontGlyphEnumerator cb, void* ctx) {
+	if (slf == nullptr || cb == nullptr) {
+		CZK_LOG_WARN_NULL("ZkFont_enumerateGlyphs");
+		return;
+	}
+
+	ZkFontGlyph fg {0, ZkVec2f {0, 0}, ZkVec2f {0, 0}};
+	for (auto& glyph : slf->glyphs) {
+		fg.width = glyph.width;
+		fg.topLeft = glyph.uv[0];
+		fg.bottomRight = glyph.uv[1];
+		if (cb(ctx, &fg)) break;
+	}
+}
