95de2441f28b972bbea6f511942ede638aa72419
Luis Michaelis <me@lmichaelis.de>
2024-01-05 10:07:15 +0100

    fix(World): broken build

--- src/World.cc
+++ src/World.cc
@@ -4,9 +4,59 @@
 
 #include "Internal.hh"
 
-ZKC_VOB_LOADER(ZkWorld);
-ZKC_VOB_PATH_LOADER(ZkWorld);
-ZKC_VFS_LOADER(ZkWorld);
+ZkWorld* ZkWorld_load(ZkRead* buf) {
+	ZKC_TRACE_FN();
+	if (buf == nullptr) {
+		ZKC_LOG_WARN_NULL("ZkWorld"
+		                  "_load");
+		return nullptr;
+	}
+	try {
+		ZkWorld* slf = new ZkWorld(std::make_shared<zenkit::World>());
+		SLF->load(buf);
+		return slf;
+	} catch (std::exception const& exc) {
+		ZKC_LOG_ERROR("ZkWorld"
+		              "_load() failed: %s",
+		              exc.what());
+		return nullptr;
+	}
+}
+
+ZkWorld* ZkWorld_loadPath(ZkString path) {
+	ZKC_TRACE_FN();
+	if (path == nullptr) {
+		ZKC_LOG_WARN_NULL("ZkWorld"
+		                  "_loadPath");
+		return nullptr;
+	}
+	try {
+		auto buf = zenkit::Read::from(path);
+
+		ZkWorld* slf = new ZkWorld(std::make_shared<zenkit::World>());
+		SLF->load(buf.get());
+
+		return slf;
+	} catch (std::exception const& exc) {
+		ZKC_LOG_ERROR("ZkWorld"
+		              "_loadPath() failed: %s",
+		              exc.what());
+		return nullptr;
+	}
+}
+
+ZkWorld* ZkWorld_loadVfs(ZkVfs* vfs, ZkString name) {
+	if (vfs == nullptr || name == nullptr) {
+		ZKC_LOG_WARN_NULL("ZkWorld"
+		                  "_loadVfs");
+		return nullptr;
+	}
+	auto node = vfs->find(name);
+	if (node == nullptr) return nullptr;
+	auto rd = node->open_read();
+	return ZkWorld_load(rd.get());
+}
+
 ZKC_DELETER(ZkWorld);
 
 ZkMesh const* ZkWorld_getMesh(ZkWorld const* slf) {
